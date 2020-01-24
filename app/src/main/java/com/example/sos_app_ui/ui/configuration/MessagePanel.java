bdcd6e9ecd9b272205c10bce3c5f8ebd1f962bcb
Blackwatch98 <57369441+Blackwatch98@users.noreply.github.com>
2020-01-24 02:22:40 +0100

    cleaning

--- app/src/main/java/com/example/sos_app_ui/ui/configuration/MessagePanel.java
+++ app/src/main/java/com/example/sos_app_ui/ui/configuration/MessagePanel.java
@@ -1,7 +1,9 @@
 package com.example.sos_app_ui.ui.configuration;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
